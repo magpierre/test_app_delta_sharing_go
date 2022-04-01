5c4b8e6c1e6ec5ba4d18b8232e141810ea26499d
Magnus Pierre <magnus.pierre@icloud.com>
2022-04-02 01:22:59 +0200

    Complete rewrite to match the new api
    Lots of changes

--- test_delta_sharing.go
+++ test_delta_sharing.go
@@ -5,12 +5,12 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 
 	delta_sharing "github.com/delta-io/delta_sharing_go"
 )
 
 func main() {
-
 	profile := flag.String("profile", "", "Path to profile for the share you would like to access")
 	tablepath := flag.String("table_path", "", "The path to the table in the form <share>.<schema>.<table>")
 	profile_path := flag.String("profile_path", "", "Profile path in the form: <profile_path>#<table_path>")
@@ -21,25 +21,48 @@ func main() {
 	fmt.Printf("tablepath: %v\n", *tablepath)
 	fmt.Printf("profile_path: %v\n", *profile_path)
 	if profile_path != nil && *profile_path != "" {
-		df := delta_sharing.LoadAsDataFrame(*profile_path)
+		df, err := delta_sharing.LoadAsDataFrame(*profile_path)
+		if err != nil {
+			log.Fatal(err)
+		}
 		fmt.Print("Table returned:\n", df.Table())
 	}
 
 	if *profile != "" {
 		fmt.Println("List tables...")
-		y := delta_sharing.NewSharingClient(context.Background(), *profile)
-		shares := y.ListShares()
-		schemas := y.ListSchemas(shares[0])
-		tables := y.ListTables(schemas[0])
-		for i, v := range tables {
-			fmt.Println("Pos", i, "Table Name:", v.Name, "Schema:", v.Schema, "Share:", v.Share)
-			r := y.RestClient.ListFilesInTable(v)
-			x, err := json.MarshalIndent(r, "", "    ")
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Printf("response: %+v\n", string(x))
+		y, err := delta_sharing.NewSharingClient(context.Background(), *profile)
+		if err != nil {
+			log.Fatal(err)
 		}
+		shares, err := y.ListShares()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, v := range shares {
+			schemas, err := y.ListSchemas(v)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, v2 := range schemas {
+				tables, err := y.ListTables(v2)
+				if err != nil {
+					log.Fatal(err)
+				}
+				for i, v := range tables {
+					fmt.Println("Pos", i, "Table Name:", v.Name, "Schema:", v.Schema, "Share:", v.Share)
+					r, err := y.RestClient.ListFilesInTable(v)
+					if err != nil {
+						log.Fatal(err)
+					}
+					x, err := json.MarshalIndent(r, "", "    ")
+					if err != nil {
+						fmt.Println(err)
+					}
+					fmt.Printf("response: %+v\n", string(x))
 
+				}
+
+			}
+		}
 	}
 }
