19f7d330697faa7e0a0daec92573d01097c38a19
Magnus Pierre <magnus.pierre@icloud.com>
2022-06-24 18:49:47 +0200

    Arrow support
    And better metadata

--- go.mod
+++ go.mod
@@ -4,28 +4,52 @@ go 1.18
 
 replace github.com/delta-io/delta_sharing_go => ../delta-sharing/golang/delta_sharing_go/
 
-require github.com/delta-io/delta_sharing_go v0.0.0-00010101000000-000000000000
+require (
+	github.com/apache/arrow/go/v8 v8.0.0-20220415204549-d36b2b3392ed
+	github.com/delta-io/delta_sharing_go v0.0.0-00010101000000-000000000000
+	github.com/rocketlaunchr/dataframe-go v0.0.0-20211025052708-a1030444159b // indirect
+)
 
 require (
-	github.com/apache/thrift v0.0.0-20181112125854-24918abba929 // indirect
-	github.com/goccy/go-json v0.7.6 // indirect
-	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
-	github.com/guptarohit/asciigraph v0.5.1 // indirect
-	github.com/juju/clock v0.0.0-20190205081909-9c5c9712527c // indirect
-	github.com/juju/errors v0.0.0-20200330140219-3fe23663418f // indirect
-	github.com/juju/loggo v0.0.0-20200526014432-9ce3a2e09b5e // indirect
-	github.com/juju/utils/v2 v2.0.0-20200923005554-4646bfea2ef1 // indirect
-	github.com/klauspost/compress v1.9.7 // indirect
-	github.com/mattn/go-runewidth v0.0.7 // indirect
-	github.com/olekukonko/tablewriter v0.0.4 // indirect
-	github.com/rocketlaunchr/dataframe-go v0.0.0-20211025052708-a1030444159b // indirect
+	github.com/JohnCGriffin/overflow v0.0.0-20211019200055-46fa312c352c // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/apache/arrow/go/arrow v0.0.0-20211112161151-bc219186db40 // indirect
+	github.com/apache/thrift v0.16.0 // indirect
+	github.com/bndr/gotabulate v1.1.2 // indirect
+	github.com/goccy/go-json v0.9.6 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/flatbuffers v2.0.5+incompatible // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/guptarohit/asciigraph v0.5.3 // indirect
+	github.com/juju/clock v0.0.0-20220203021603-d9deb868a28a // indirect
+	github.com/juju/errors v0.0.0-20220331221717-b38fca44723b // indirect
+	github.com/juju/loggo v0.0.0-20210728185423-eebad3a902c4 // indirect
+	github.com/juju/utils/v2 v2.0.0-20210305225158-eedbe7b6b3e2 // indirect
+	github.com/klauspost/asmfmt v1.3.2 // indirect
+	github.com/klauspost/compress v1.15.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8 // indirect
+	github.com/minio/c2goasm v0.0.0-20190812172519-36a3d3bbc4f3 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rocketlaunchr/mysql-go v1.1.3 // indirect
-	github.com/xitongsys/parquet-go v1.5.2 // indirect
+	github.com/xitongsys/parquet-go v1.6.2 // indirect
 	github.com/xitongsys/parquet-go-source v0.0.0-20220315005136-aec0fe3e777c // indirect
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
-	golang.org/x/exp v0.0.0-20200331195152-e8c3332aa8e5 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	github.com/zeebo/xxh3 v1.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
+	golang.org/x/exp v0.0.0-20220328175248-053ad81199eb // indirect
+	golang.org/x/mod v0.6.0-dev.0.20211013180041-c96bc1413d57 // indirect
+	golang.org/x/net v0.0.0-20220403103023-749bd193bc2b // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.9 // indirect
+	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
+	google.golang.org/genproto v0.0.0-20220126215142-9970aeb2e350 // indirect
+	google.golang.org/grpc v1.44.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- test_delta_sharing.go
+++ test_delta_sharing.go
@@ -2,35 +2,103 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
+	"github.com/apache/arrow/go/v8/arrow"
+	"github.com/apache/arrow/go/v8/arrow/array"
+	"github.com/bndr/gotabulate"
 	delta_sharing "github.com/delta-io/delta_sharing_go"
 )
 
 func main() {
-	profile := flag.String("profile", "", "Path to profile for the share you would like to access")
-	tablepath := flag.String("table_path", "", "The path to the table in the form <share>.<schema>.<table>")
+	prof := flag.String("profile", "", "Path to profile for the share you would like to access")
 	profile_path := flag.String("profile_path", "", "Profile path in the form: <profile_path>#<table_path>")
-
 	flag.Parse()
-
-	fmt.Printf("profile: %v\n", *profile)
-	fmt.Printf("tablepath: %v\n", *tablepath)
-	fmt.Printf("profile_path: %v\n", *profile_path)
 	if profile_path != nil && *profile_path != "" {
-		df, err := delta_sharing.LoadAsDataFrame(*profile_path)
-		if err != nil {
-			log.Fatal(err)
+		x, _ := delta_sharing.LoadAsArrowTable(*profile_path, 0)
+		if x == nil {
+			log.Fatal("No arrow table")
 		}
-		fmt.Print("Table returned:\n", df.Table())
+		at := x.(array.Table)
+		fmt.Println("Arrow", at.Schema())
+		tr := array.NewTableReader(at, 100)
+		tr.Retain()
+		fmt.Println("")
+		fmt.Println("Data:")
+		var tbl [][]interface{}
+		h := []string{}
+		for i := 0; i < int(at.NumCols()); i++ {
+			h = append(h, at.Column(i).Name())
+		}
+
+		for tr.Next() {
+			rec := tr.Record()
+			for pos := 0; pos < int(rec.NumRows()); pos++ {
+				var d []interface{}
+				for _, col := range rec.Columns() {
+					switch col.DataType().ID() {
+					case arrow.STRING:
+						a := col.(*array.String)
+						d = append(d, a.Value(pos))
+					case arrow.INT16:
+						i16 := col.(*array.Int16)
+						d = append(d, int(i16.Value(pos)))
+					case arrow.INT32:
+						i32 := col.(*array.Int32)
+						d = append(d, int(i32.Value(pos)))
+					case arrow.INT64:
+						i64 := col.(*array.Int64)
+						d = append(d, int(i64.Value(pos)))
+					case arrow.FLOAT16:
+						f16 := col.(*array.Float16)
+						d = append(d, f16.Value(pos))
+					case arrow.FLOAT32:
+						f32 := col.(*array.Float32)
+						d = append(d, f32.Value(pos))
+					case arrow.FLOAT64:
+						f64 := col.(*array.Float64)
+						d = append(d, f64.Value(pos))
+					case arrow.BOOL:
+						b := col.(*array.Boolean)
+						d = append(d, b.Value(pos))
+					case arrow.BINARY:
+						bi := col.(*array.Binary)
+						d = append(d, bi.Value(pos))
+					case arrow.DATE32:
+						d32 := col.(*array.Date32)
+						d = append(d, d32.Value(pos))
+					case arrow.DATE64:
+						d64 := col.(*array.Date64)
+						d = append(d, d64.Value(pos))
+					case arrow.DECIMAL128:
+						dec := col.(*array.Decimal128)
+						d = append(d, dec.Value(pos))
+					case arrow.INTERVAL_DAY_TIME:
+						idt := col.(*array.DayTimeInterval)
+						d = append(d, idt.Value(pos))
+					}
+				}
+				tbl = append(tbl, d)
+
+			}
+		}
+		tr.Release()
+		f := gotabulate.Create(tbl)
+		f.SetHeaders(h)
+		f.SetAlign("left")
+		f.SetEmptyString("None")
+		f.SetWrapStrings(true)
+		f.SetMaxCellSize(25)
+		fmt.Println(f.Render("grid"))
 	}
 
-	if *profile != "" {
+	/* Returning metadata from the Delta Sharing server */
+	if *prof != "" {
 		fmt.Println("List tables...")
-		y, err := delta_sharing.NewSharingClient(context.Background(), *profile)
+		y, err := delta_sharing.NewSharingClient(context.Background(), *prof)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -49,16 +117,33 @@ func main() {
 					log.Fatal(err)
 				}
 				for i, v := range tables {
-					fmt.Println("Pos", i, "Table Name:", v.Name, "Schema:", v.Schema, "Share:", v.Share)
+					fmt.Printf("Pos %d,table path %s.%s.%s\n", i, v.Share, v.Schema, v.Name)
 					r, err := y.RestClient.ListFilesInTable(v)
 					if err != nil {
 						log.Fatal(err)
 					}
-					x, err := json.MarshalIndent(r, "", "    ")
+
+					val, err := r.Metadata.GetSparkSchema()
 					if err != nil {
-						fmt.Println(err)
+						log.Fatal(err)
 					}
-					fmt.Printf("response: %+v\n", string(x))
+
+					for _, f := range val.Fields {
+						fmt.Printf("Schema Field:%s|%s%s|%s%t\n", strings.ToUpper(f.Name), "Datatype:", f.Type, "Nullable:", f.Nullable)
+					}
+					fmt.Println("Stored statistics:")
+					for _, v := range r.AddFiles {
+						s, err := v.GetStats()
+						if err == nil {
+							fmt.Println("Number of records", s.NumRecords)
+							for k, value := range s.MinValues {
+								fmt.Println(k, "|", value, "|", s.MaxValues[k], "|", s.NullCount[k])
+							}
+						} else {
+							fmt.Printf("No stats available for file with id: %s\n", v.Id)
+						}
+
+					}
 
 				}
 
